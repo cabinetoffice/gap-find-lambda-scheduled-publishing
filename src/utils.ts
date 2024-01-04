04f9f75bbfb78b829b3d3800f45f6a2f6f96979d
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2024-01-04 16:37:56 +0000

    add encryption

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import NodeRSA from 'node-rsa';
 export const delay = (milliseconds: number) => {
   return new Promise((resolve) => {
     setTimeout(resolve, milliseconds);
@@ -8,11 +9,14 @@ export const getMilliSecondsBetween = (before: Date, after: Date) => {
   return after.getTime() - before.getTime();
 };
 
-export const calculateTimeToWait = (
-  minTime: number,
-  before: Date,
-  after: Date
-) => {
+export const calculateTimeToWait = (minTime: number, before: Date, after: Date) => {
   const differenceInMilliseconds = getMilliSecondsBetween(after, before);
   return minTime + differenceInMilliseconds;
+};
+
+export const encrypt = (data: string, publicKey: string): string => {
+  const key = new NodeRSA();
+  key.importKey(publicKey, 'pkcs8-public-pem');
+
+  return key.encrypt(data, 'base64');
 };
