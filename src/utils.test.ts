0db401b94e2b5483f52bff73d4a54eee248758e9
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2024-01-08 16:05:23 +0000

    fix test

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -81,17 +81,7 @@ describe('utils', () => {
 
   describe('encrypt function', () => {
     it('should encrypt data using the provided public key', () => {
-      const mockPublicKey = `
-        -----BEGIN PUBLIC KEY-----
-        MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7e7oyeyA5S6v9SgFIoDi
-        6kuzsFuk4U2DZUVjjRjwjX7OS/Wdxhb66HODm0J4lgvj2jb3wxjkgLyRlxdnKLUe
-        DkOpZ37HgUzbdNc83aQwt6ZpBB/xskc+L5d/JAQTmI3eWRld+edkGROeTk9t9AMO
-        I8Xan9dI7XpA5D+eZAxLCX2s0eOqRLD1YB4D+lcj+R0aFj8aFFxf6bLOVpNt9/Sy
-        4I74sDkxDh19WmZwM1tQADK93yXHTeDKE0jqKmbT6Do0A7m+/68UDsbImQuZ0wZB
-        Nnl2Bb5qOWeWvOLoFJwMio+Go1ScyZwrtQyl2A9pw+fUGd5BuK2UGkxGOUgH+H0W
-        xwIDAQAB
-        -----END PUBLIC KEY-----
-      `;
+      const mockPublicKey = `MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7e7oyeyA5S6v9SgFIoDi6kuzsFuk4U2DZUVjjRjwjX7OS/Wdxhb66HODm0J4lgvj2jb3wxjkgLyRlxdnKLUeDkOpZ37HgUzbdNc83aQwt6ZpBB/xskc+L5d/JAQTmI3eWRld+edkGROeTk9t9AMOI8Xan9dI7XpA5D+eZAxLCX2s0eOqRLD1YB4D+lcj+R0aFj8aFFxf6bLOVpNt9/Sy4I74sDkxDh19WmZwM1tQADK93yXHTeDKE0jqKmbT6Do0A7m+/68UDsbImQuZ0wZBNnl2Bb5qOWeWvOLoFJwMio+Go1ScyZwrtQyl2A9pw+fUGd5BuK2UGkxGOUgH+H0WxwIDAQAB`;
 
       const testData = 'Hello, World!';
       const encryptedData = encrypt(testData, mockPublicKey);
